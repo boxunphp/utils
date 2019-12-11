33f6a882fd39a97d1e523107dde0114237555e2a
Jordy <arno.zheng@gmail.com>
2019-12-11 11:34:37 +0800

    init: 增加http code message

--- HttpCode.php
+++ HttpCode.php
@@ -18,16 +18,13 @@ class HttpCode
     const NO_CONTENT = 204;
     const RESET_CONTENT = 205;
     const PARTIAL_CONTENT = 206;
-    const MULTI_STATUS = 207;
 
     // 重定向
     const MULTIPLE_CHOICES = 300;
     const MOVED_PERMANENTLY = 301;
-    const MOVE_TEMPORARILY = 302;
+    const FOUND = 302;
     const SEE_OTHER = 303;
     const NOT_MODIFIED = 304;
-    const USE_PROXY = 305;
-    const SWITCH_PROXY = 306;
     const TEMPORARY_REDIRECT = 307;
 
     // 请求错误
@@ -50,14 +47,8 @@ class HttpCode
     const REQUESTED_RANGE_NOT_SATISFIABLE = 416;
     const EXPECTATION_FAILED = 417;
     const IM_A_TEAPOT = 418;
-    const TOO_MANY_CONNECTIONS = 421;
-    const UNPROCESSABLE_ENTITY = 422;
-    const LOCKED = 423;
-    const FAILED_DEPENDENCY = 424;
-    const UNORDERED_COLLECTION = 425;
-    const UPGRADE_REQUIRED = 426;
-    const RETRY_WITH = 449;
-    const UNAVAILABLE_FOR_LEGAL_REASONS = 400;
+    const TOO_MANY_REQUESTS = 421;
+    const REQUEST_HEADER_FIELDS_TOO_LARGE = 431;
 
     // 服务器错误
     const INTERNAL_SERVER_ERROR = 500;
@@ -66,9 +57,59 @@ class HttpCode
     const SERVICE_UNAVAILABLE = 503;
     const GATEWAY_TIMEOUT = 504;
     const HTTP_VERSION_NOT_SUPPORTED = 505;
-    const VARIANT_ALSO_NEGOTIATES = 506;
-    const INSUFFICIENT_STORAGE = 507;
-    const BANDWIDTH_LIMIT_EXCEEDED = 509;
-    const NOT_EXTENDED = 510;
-    const UNPARSEABLE_RESPONSE_HEADERS = 600;
+
+    protected static $messageConfig = [
+        // 成功
+        self::OK => 'OK',
+        self::CREATED => 'Created',
+        self::ACCEPTED => 'Accepted',
+        self::NON_AUTHORITATIVE_INFORMATION => 'Non-Authoritative Information',
+        self::NO_CONTENT => 'No Content',
+        self::RESET_CONTENT => 'Reset Content',
+        self::PARTIAL_CONTENT => 'Partial Content',
+
+        // 重定向
+        self::MULTIPLE_CHOICES => 'Multiple Choice',
+        self::MOVED_PERMANENTLY => 'Moved Permanently',
+        self::FOUND => 'Found',
+        self::SEE_OTHER => 'See Other',
+        self::NOT_MODIFIED => 'Not Modified',
+        self::TEMPORARY_REDIRECT => 'Temporary Redirect',
+
+        // 请求错误
+        self::BAD_REQUEST => 'Bad Request',
+        self::UNAUTHORIZED => 'Unauthorized',
+        self::PAYMENT_REQUIRED => 'Payment Required',
+        self::FORBIDDEN => 'Forbidden',
+        self::NOT_FOUND => 'Not Found',
+        self::METHOD_NOT_ALLOWED => 'Method Not Allowed',
+        self::NOT_ACCEPTABLE => 'Not Acceptable',
+        self::PROXY_AUTHENTICATION_REQUIRED => 'Proxy Authentication Required',
+        self::REQUEST_TIMEOUT => 'Request Timeout',
+        self::CONFLICT => 'Conflict',
+        self::GONE => 'Gone',
+        self::LENGTH_REQUIRED => 'Length Required',
+        self::PRECONDITION_FAILED => 'Precondition Failed',
+        self::REQUEST_ENTITY_TOO_LARGE => 'Payload Too Large',
+        self::REQUEST_URI_TOO_LONG => 'URI Too Long',
+        self::UNSUPPORTED_MEDIA_TYPE => 'Unsupported Media Type',
+        self::REQUESTED_RANGE_NOT_SATISFIABLE => 'Requested Range Not Satisfiable',
+        self::EXPECTATION_FAILED => 'Expectation Failed',
+        self::IM_A_TEAPOT => 'I\'m a teapot',
+        self::TOO_MANY_REQUESTS => 'Too Many Requests',
+        self::REQUEST_HEADER_FIELDS_TOO_LARGE => 'Request Header Fields Too Large',
+
+        // 服务器错误
+        self::INTERNAL_SERVER_ERROR => 'Internal Server Error',
+        self::NOT_IMPLEMENTED => 'Not Implemented',
+        self::BAD_GATEWAY => 'Bad Gateway',
+        self::SERVICE_UNAVAILABLE => 'Service Unavailable',
+        self::GATEWAY_TIMEOUT => 'Gateway Timeout',
+        self::HTTP_VERSION_NOT_SUPPORTED => 'HTTP Version Not Supported',
+    ];
+
+    public static function message($code)
+    {
+        return empty(self::$messageConfig[$code]) ? '' : self::$messageConfig[$code];
+    }
 }
